9d9b7a3e49bfac2fe2fe8c726d494e0e1a08c22c
George Wilson <george.wilson@delphix.com>
2018-03-09 21:05:20 -0500

    OpenZFS 9102 - zfs should be able to initialize...
    storage devices
    
    Authored by: George Wilson <george.wilson@delphix.com>
    Reviewed by: John Wren Kennedy <john.kennedy@delphix.com>
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: Prakash Surya <prakash.surya@delphix.com>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    Signed-off-by: Tim Chase <tim@chase2k.com>
    Ported-by: Tim Chase <tim@chase2k.com>
    OpenZFS-commit: https://github.com/openzfs/openzfs/commit/c3963210eb877c0bfb01c9ce117d0e7c1ac272e4
    OpenZFS-issue: https://www.illumos.org/issues/9102
    
    PROBLEM
    ========
    
    The first access to a block incurs a performance penalty on some platforms
    (e.g. AWS's EBS, VMware VMDKs). Therefore we recommend that volumes are "thick
    provisioned", where supported by the platform (VMware). This can create a large
    delay in getting a new virtual machines up and running (or adding storage to
    an existing Engine). If the thick provision step is omitted, write  performance
    will be suboptimal until all blocks on the LUN have been written.
    
    SOLUTION
    =========
    
    This feature introduces a way to 'initialize' the disks at install or in the
    background to make sure we don't incur this first read penalty.
    
    When an entire LUN is added to ZFS, we make all space available immediately,
    and allow ZFS to find unallocated space and zero it out. This works with
    concurrent writes to arbitrary offsets, ensuring that we don't zero out
    something that has been (or is in the middle of being) written. This scheme
    can also be applied to existing pools (affecting only free regions on the
    vdev). Detailed design:
            - new subcommand:zpool initialize [-cs] <pool> [<vdev> ...]
                    - start, suspend, or cancel initialization
            - Creates new open-context thread for each vdev
            - Thread iterates through all metaslabs in this vdev
            - Each metaslab:
                    - select a metaslab
                    - load the metaslab
                    - mark the metaslab as being zeroed
                    - walk all free ranges within that metaslab and translate them
                      to ranges on the leaf vdev
                    - issue a "zeroing" I/O on the leaf vdev that corresponds to a
                      free range on the metaslab we're working on
                    - continue until all free ranges for this metaslab have been
                      "zeroed"
                    - reset/unmark the metaslab being zeroed
                    - if more metaslabs exist, then repeat above tasks.
                    - if no more metaslabs, then we're done.
    
            - progress for the initialization is stored on-disk in the vdev’s leaf
              zap object. The following information is stored:
                    - the last offset that has been initialized
                    - the state of the initialization process (i.e. active,
                      suspended, or canceled)
                    - the start time for the initialization
    
            - progress is reported via the zpool status command and shows
              information for each of the videos that are initializing
    
    Porting notes:
    
        Added zfs_initialize_value module parameter to set the pattern
        written by "zpool initialize".

--- include/sys/fs/zfs.h
+++ include/sys/fs/zfs.h
@@ -758,6 +758,13 @@ typedef struct zpool_load_policy {
 #define	VDEV_TOP_ZAP_POOL_CHECKPOINT_SM \
 	"com.delphix:pool_checkpoint_sm"
 
+#define	VDEV_LEAF_ZAP_INITIALIZE_LAST_OFFSET	\
+	"com.delphix:next_offset_to_initialize"
+#define	VDEV_LEAF_ZAP_INITIALIZE_STATE	\
+	"com.delphix:vdev_initialize_state"
+#define	VDEV_LEAF_ZAP_INITIALIZE_ACTION_TIME	\
+	"com.delphix:vdev_initialize_action_time"
+
 /*
  * This is needed in userland to report the minimum necessary device size.
  */
@@ -972,9 +979,14 @@ typedef struct vdev_stat {
 	uint64_t	vs_write_errors;	/* write errors		*/
 	uint64_t	vs_checksum_errors;	/* checksum errors	*/
 	uint64_t	vs_self_healed;		/* self-healed bytes	*/
+	uint64_t	vs_initialize_errors;	/* initializing errors	*/
 	uint64_t	vs_scan_removing;	/* removing?	*/
 	uint64_t	vs_scan_processed;	/* scan processed bytes	*/
 	uint64_t	vs_fragmentation;	/* device fragmentation */
+	uint64_t	vs_initialize_bytes_done; /* bytes initialized */
+	uint64_t	vs_initialize_bytes_est; /* total bytes to initialize */
+	uint64_t	vs_initialize_state;	/* vdev_initialzing_state_t */
+	uint64_t	vs_initialize_action_time; /* time_t */
 	uint64_t	vs_checkpoint_space;    /* checkpoint-consumed space */
 } vdev_stat_t;
 
@@ -1004,7 +1016,6 @@ typedef struct vdev_stat_ex {
 #define	VDEV_L_HISTO_BUCKETS 37		/* Latency histo buckets */
 #define	VDEV_RQ_HISTO_BUCKETS 25	/* Request size histo buckets */
 
-
 	/* Amount of time in ZIO queue (ns) */
 	uint64_t vsx_queue_histo[ZIO_PRIORITY_NUM_QUEUEABLE]
 	    [VDEV_L_HISTO_BUCKETS];
@@ -1030,6 +1041,16 @@ typedef struct vdev_stat_ex {
 	    [VDEV_RQ_HISTO_BUCKETS];
 
 } vdev_stat_ex_t;
+
+/*
+ * Initialize functions.
+ */
+typedef enum pool_initialize_func {
+	POOL_INITIALIZE_DO,
+	POOL_INITIALIZE_CANCEL,
+	POOL_INITIALIZE_SUSPEND,
+	POOL_INITIALIZE_FUNCS
+} pool_initialize_func_t;
 
 /*
  * DDT statistics.  Note: all fields should be 64-bit because this
@@ -1074,6 +1095,14 @@ typedef struct ddt_histogram {
 
 #define	ZVOL_PROP_NAME		"name"
 #define	ZVOL_DEFAULT_BLOCKSIZE	8192
+
+typedef enum {
+	VDEV_INITIALIZE_NONE,
+	VDEV_INITIALIZE_ACTIVE,
+	VDEV_INITIALIZE_CANCELED,
+	VDEV_INITIALIZE_SUSPENDED,
+	VDEV_INITIALIZE_COMPLETE
+} vdev_initializing_state_t;
 
 /*
  * /dev/zfs ioctl numbers.
@@ -1163,6 +1192,7 @@ typedef enum zfs_ioc {
 	ZFS_IOC_REMAP,
 	ZFS_IOC_POOL_CHECKPOINT,
 	ZFS_IOC_POOL_DISCARD_CHECKPOINT,
+	ZFS_IOC_POOL_INITIALIZE,
 
 	/*
 	 * Linux - 3/64 numbers reserved.
@@ -1249,6 +1279,12 @@ typedef enum {
  * history log.
  */
 #define	ZPOOL_HIDDEN_ARGS	"hidden_args"
+
+/*
+ * The following are names used when invoking ZFS_IOC_POOL_INITIALIZE.
+ */
+#define	ZPOOL_INITIALIZE_COMMAND	"initialize_command"
+#define	ZPOOL_INITIALIZE_VDEVS		"initialize_vdevs"
 
 /*
  * Flags for ZFS_IOC_VDEV_SET_STATE
