25e3d31f8dacf8e0a8de3c55fa6f215374b5f1c8
Brian Behlendorf <behlendorf1@llnl.gov>
2020-03-06 15:43:06 -0800

    Fix of by 1 error in spares calulation.
    - Several ZTS tests were failing due to an off by one error when
      calculating the allowing number of spares.
    - Fix zpool_create_draid_004_neg test case name.
    - Use truncate for rewind test cases.
    - Remove extraneous ASSERT.
    - Remove cmd/draidcfg/.gitignore.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -5728,7 +5728,6 @@ top:
 		 * Gracefully handle a damaged logical block size as a
 		 * checksum error.
 		 */
-		ASSERT3U(lsize, <=, spa_maxblocksize);
 		if (lsize > spa_maxblocksize(spa)) {
 			rc = SET_ERROR(ECKSUM);
 			if (hash_lock != NULL)
